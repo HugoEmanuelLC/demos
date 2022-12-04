a1dd14f3762e9387298fb83e9e89c2305c9f6ec0
hugo <helc85.info@gmail.com>
2022-12-04 19:05:07 +0100

    developpement demos model mark01

--- models/mark01/assets/js/scrolls_animation.js
+++ models/mark01/assets/js/scrolls_animation.js
@@ -5,14 +5,14 @@ $(function(){
     let html_body = $('html, body');
     let footer = $('footer');
     let a_scroll_up = $(".a_scroll_up");
-    let a_footer = $(".a_footer");
+    let a_scroll_down = $(".a_scroll_down");
 
     a_scroll_up.click(function(){
         html_body.animate({
             scrollTop: html_body.offset().top
         }, 2000);
     })
-    a_footer.click(function(){
+    a_scroll_down.click(function(){
         html_body.animate({
             scrollTop: footer.offset().top
         }, 2000);
