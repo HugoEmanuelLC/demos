d20f91191b9f8cf6a5b5a2829182d5b2c2f5d4ed
hugo <helc85.info@gmail.com>
2022-12-04 18:14:31 +0100

    developpement demos model mark01

--- models/mark01/assets/js/scrolls_animation.js
+++ models/mark01/assets/js/scrolls_animation.js
@@ -3,17 +3,18 @@
 // BIBLIOTHEQUE JQUERY
 $(function(){
     let html_body = $('html, body');
+    let footer = $('footer');
     let a_scroll_up = $(".a_scroll_up");
-    let a_apropos = $(".a_apropos");
+    let a_footer = $(".a_footer");
 
     a_scroll_up.click(function(){
         html_body.animate({
             scrollTop: html_body.offset().top
         }, 2000);
     })
-    a_apropos.click(function(){
+    a_footer.click(function(){
         html_body.animate({
-            scrollTop: $("#apropos").offset().top
+            scrollTop: footer.offset().top
         }, 2000);
     })
 })
